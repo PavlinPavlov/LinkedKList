7a786b22694e7594254607a60340099cc99ae2aa
Pavlin Pavlov <888pavlin888@gmail.com>
2020-04-05 23:00:59 +0300

    Insert at method implemented

--- LinkedKList.py
+++ LinkedKList.py
@@ -32,11 +32,41 @@ class KList:
             self.first_free_kth_element.next_kth = new_node
             self.first_free_kth_element = self.first_free_kth_element.next
 
+    def insert_at(self, data, insert_position):
+        new_node = KNode(data)
+
+        if 0 == insert_position:
+            new_node.next = self.head
+            new_node.next_kth = self.search(self.k - 1)
+            self.head = new_node
+
+        current_index = 0
+        first_alter_index = insert_position - self.k
+        last_alter_index = insert_position - 1
+
+        current_element = self.head
+        previous = None
+        while current_element:
+            if (current_index > first_alter_index) & (current_index < last_alter_index):
+                current_element.next_kth = previous.next_kth
+
+            if current_index == first_alter_index:
+                current_element.next_kth = new_node
+
+            if current_index == last_alter_index:
+                new_node.next = current_element.next
+                current_element.next = new_node
+                break
+
+            current_index += 1
+            previous = current_element
+            current_element = current_element.next
+
     def search(self, position):
         normal_search_count = position % self.k
-        k_search_count = position // self.k
-        print("Debug - K-th searches:", k_search_count)
-        print("Debug - Normal searches:", normal_search_count)
+        k_search_count = position // self.k  # integer division
+        print("Info - K-th searches:", k_search_count)
+        print("Info - Normal searches:", normal_search_count)
 
         current_element = self.head
 
@@ -55,29 +85,29 @@ class KList:
         return current_element.data
 
     def remove_at(self, remove_position):
-        if remove_position >= self.size:
+        if remove_position >= self.size | remove_position < 0:
             return "The list does not have a element at position " + str(remove_position)
 
         if remove_position == 0:
             self.head = self.head.next
 
-        curr_index = 0
-        first_to_alter_index = remove_position - self.k
-        last_to_alter_index = remove_position - 1
+        current_index = 0
+        first_alter_index = remove_position - self.k
+        last_alter_index = remove_position - 1
 
         current_element = self.head
         while current_element:
-            if (curr_index >= first_to_alter_index) & (curr_index < last_to_alter_index):
+            if (current_index >= first_alter_index) & (current_index < last_alter_index):
                 if current_element.next_kth is not None:
                     current_element.next_kth = current_element.next_kth.next
 
-            if curr_index == last_to_alter_index:
+            if current_index == last_alter_index:
                 if current_element.next_kth is not None:
                     current_element.next_kth = current_element.next_kth.next
                 current_element.next = current_element.next.next
                 break
 
-            curr_index += 1
+            current_index += 1
             current_element = current_element.next
 
         return "Done"
