29a26ecd17e83f4a2d243533485de3e009a51e16
Pavlin Pavlov <888pavlin888@gmail.com>
2020-04-07 20:22:06 +0300

    Reader updated to use new list creation

--- Reader.py
+++ Reader.py
@@ -4,19 +4,18 @@ from LinkedKList import KList
 from LinkedList import List
 
 
-def make_list_from_file(path="binfile.bin", k=1):
-    l_list = KList(k) if k > 1 else List()  # chooses list implementation
-    f = open(path, "rb")
-    for num in list(f.read()):
-        l_list.insert(num)
-    f.close()
+def make_list_from_file(path="binfile.bin", k=10):
+    file_d = open(path, "rb")
+    file_content_list = list(file_d.read())
+    l_list = KList(k, file_content_list) if k > 1 else List(file_content_list)
+    file_d.close()
     return l_list
 
 
-def create_file():
-    f = open("binfile.bin", "wb")
+def create_file(nums=5000000, path="binfile.bin"):
+    file_d = open(path, "wb")
     generated_values = []
-    for _ in range(500):
+    for _ in range(nums):
         generated_values.append(randint(10, 255))
-    f.write(bytearray(generated_values))
-    f.close()
+    file_d.write(bytearray(generated_values))
+    file_d.close()
